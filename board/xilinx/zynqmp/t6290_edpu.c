57ac729744c92dfc92b3b7eeaf377acbfbabe1c6
dpirly <dpirly@126.com>
2018-11-22 21:49:11 +0800

    [edpu] reading DDR4 SPD inforamation 1. display DDR4 infor 2. genearate variable by DDR4 size and rank 3. download different bit or ub file

--- board/xilinx/zynqmp/t6290_edpu.c
+++ board/xilinx/zynqmp/t6290_edpu.c
@@ -337,6 +337,18 @@ uint8_t config_dsp2_arr[]=
 
 #define		DSP_EEPROM_ADDR (0x50)
 
+static char* to_lower(char* s){
+	char* r = s;
+	char c;
+	while (*s != '\0'){
+		c = *s;
+		if(c >= 'A' && c <= 'Z'){
+			*s = c + 0x20;
+		}
+		s++;
+	}
+	return r;
+}
 static void edpu_dsp_select(uint32_t dsp_id){
 	if (dsp_id == 0)
 		i2c_set_bus_num(2); /*pca9548a channel 1 */
@@ -454,6 +466,179 @@ static int edpu_dsp_config(uint32_t dsp_id, uint32_t force){
 
 	return 0;
 }
+
+/*
+	ZynqMP> i2c bus
+	Bus 0:	zynq_1
+	Bus 1:	zynq_1->PCA9548@0x70:0 (PS-PMBUS)
+	Bus 2:	zynq_1->PCA9548@0x70:1 (DSP1-EEPROM: 0x50) --!!
+	Bus 3:	zynq_1->PCA9548@0x70:2 (DSP2-EEPROM: 0x50) --!!
+	Bus 4:	zynq_1->PCA9548@0x70:3 (ADT75: 0x48)
+	Bus 5:	zynq_1->PCA9548@0x70:4 (PS-DDR4 SPD)
+	Bus 6:	zynq_1->PCA9548@0x70:5 (PL-DDR4 SPD)
+	Bus 7:	zynq_1->PCA9548@0x70:6 (PS-USB-SDA)
+	Bus 8:	zynq_1->PCA9548@0x70:7 (QSFP+)
+*/
+
+static bool edpu_ddr4_select_page(uint8_t page){
+	uint8_t dummy;
+	int ret;
+	
+	if(page == 0){
+		ret = i2c_write(0x36, 0, 0, &dummy, 1);
+	}else{
+		ret = i2c_write(0x37, 0, 0, &dummy, 0);
+	}
+	return ret;
+}
+
+static const char* edpu_ddr4_company(uint8_t bank, uint8_t code){
+	static char unknown[32];
+	if (bank == 1 && code == 0x2c){
+		return "Micron Technology";
+	}else if (bank == 1 && code == 0xad){
+		return "SK Hynix";
+	}else if (bank == 2 && code == 0x98){
+		return "Kingston";
+	}else if(bank == 5 && code == 0xcb){
+		return "A-DATA Technology";
+	}else if(bank == 3 && code == 0x9E){
+		return "Corsair";
+	}else if(bank == 8 && code == 0x46){
+		return "Gloway International";
+	}else if(bank == 9 && code == 0x13){
+		return "Gloway International";
+	}else if((bank == 0x80 || bank == 0x00) && code == 0xCE){
+		return "Samsung";
+	}else if(bank == 5 && code == 0xCD){
+		return "G.Skill Intl";
+	}else if(bank == 10 && code == 0x68){
+		return "Kimtigo Semiconductor";
+	}else if(bank == 5 && code == 0xef){
+		return "Team Group Inc";
+	}else if(bank == 6 && code == 0x9b){
+		return "Crucial Technology";
+	}else if(bank == 9 && code == 0xc8){
+		return "Lenovo";
+	}else if(bank == 2 && code == 0x7a){
+		return "Apacer Technology";
+	}
+	sprintf(unknown, "Unknown(bank=%d,code=%02x)", bank, code);
+	return unknown;
+}
+
+static void edpu_ddr4_probe(const char *prefix){
+	static uint8_t spd[512];
+	char variable[32] = {0};
+	char value[32] = {0};
+	uint32_t offset;
+	
+	uint32_t rank;
+	uint32_t cap;
+	uint32_t bus_width;
+	uint32_t sdram_width;
+	uint32_t total_memory;
+
+	int8_t tckmin_mtb;
+	int8_t tckmin_offset_ftb;
+	float tckavg_min_result;
+	float speed;
+
+	/* read all page 0 contents */
+	if(edpu_ddr4_select_page(0) != 0){
+		printf("%s: None!\n", prefix);
+		return;
+	}
+	for(offset=0; offset<256; offset+=32){
+		i2c_read(0x51, 			/* chip address */
+				offset, 				/* byte address */
+				1, 							/* address length */
+				&spd[offset], 	/* buffer */
+				32); 						/* read length */
+	}
+
+	/* read all page 1 contents */
+	if(edpu_ddr4_select_page(1) != 0){
+		printf("%s: error!\n", prefix);
+		return;
+	}
+	for(offset=0; offset<256; offset+=32){
+		i2c_read(0x51,			/* chip address */
+				offset, 				/* byte address */
+				1,							/* address length */
+				&spd[256+offset],/* buffer, have offset */
+				32);						/* read length */
+	}
+
+	/* parse SPD content */
+	if(spd[0x02] != 0x0c){
+		printf("%s-DDR is not DDR4, except 0x0c, got 0x%x\n", prefix, spd[0x02]);
+		return;
+	}
+	if(spd[0x03] != 0x03){
+		printf("%s-DDR is not SO-DIMM, except 0x03, got 0x%x\n", prefix, spd[0x03]);
+		return;
+	}
+	if(spd[0x06] != 0x00){
+		printf("%s-DDR is unknown package type, except 0x00, got 0x%x\n", prefix, spd[0x06]);
+		return;
+	}
+
+	printf("%s: %s, ", prefix, edpu_ddr4_company((spd[0x140] & 0x7f)+1, spd[0x141]));
+	//printf("ID CODE:%02x%02x => bank%d, code:%02x\n", spd[0x140], spd[0x141],  (spd[0x140] & 0x7f)+1, spd[0x141]);
+
+	// calc speed
+	//Tckavg_min Result = Tck_min MTB(reg0x12) + Tck_min Offset FTB(reg0x7d)
+	tckmin_mtb = (int8_t)spd[0x12];
+	tckmin_offset_ftb = (int8_t)spd[0x7d];
+	tckavg_min_result = tckmin_mtb*0.125 + tckmin_offset_ftb*0.001;
+	speed = (2.0* (1/tckavg_min_result))*1000.0;
+
+	// calc capcatity
+	rank = ((spd[0x0c] & 0b111000) >> 3) + 1; /* 1 2 3 4 */
+	bus_width = (spd[0x0d] & 0b111); 
+	bus_width = 8 << bus_width; /* 8 16 32 64*/
+	sdram_width = (spd[0x0c] & 0b111);
+	sdram_width = 4 << sdram_width; /* 4 8 16 32 */
+	cap = (spd[0x04] & 0b1111);
+	cap = 256 << cap;
+	/*  Total = SDRAM Capacity / 8 * Primary Bus Width / SDRAM Widht * Logical Ranks per DIMM */
+	//total_memory = cap / 8 * bus_width / sdram_width * rank;
+	total_memory = (cap * bus_width * rank) / (8*sdram_width);
+	
+	printf("%4dMB-%dMHz, Rank %d, Bus width %d, SDRAM width %d, SDRAM capcatity %dMb, ", 
+			total_memory, (uint32_t)speed, rank, bus_width, sdram_width, cap);
+
+	memcpy(value, &spd[0x149], 20);
+	printf("PN:%s\n", value);
+
+	/* export variable */
+	sprintf(variable, "ddr_%s_size", prefix);
+	sprintf(value, "%d", total_memory);
+	env_set(to_lower(variable), value);
+
+	sprintf(variable, "ddr_%s_rank", prefix);
+	sprintf(value, "%d", rank);
+	env_set(to_lower(variable), value);
+
+	sprintf(variable, "ddr_%s_speed", prefix);
+	sprintf(value, "%d", (uint32_t)speed);
+	env_set(to_lower(variable), value);
+}
+
+static void edpu_ddr4_init(void){
+	printf("\nMemory:\n");
+
+	/* PS-DDR4 */
+	i2c_set_bus_num(5); /*pca9548a channel 4 */
+	i2c_init(100000, 0x51);
+	edpu_ddr4_probe("PS");
+
+	/* PL-DDR4 */
+	i2c_set_bus_num(6); /*pca9548a channel 5 */
+	edpu_ddr4_probe("PL");
+}
+
 static int edpu_spi_xfer(uint32_t dout, uint32_t *din)
 {
 	unsigned int bus = 1;
@@ -754,12 +939,17 @@ void edpu_init_eth_mac(void){
 
 void edpu_cpld_init(void)
 {
-	printf("\neDPU CPLD:%s\n", edpu_cpld_version());
+	/* check DDR4 module */
+	edpu_ddr4_init();
 
-	/* put all dsp into reset */
-	edpu_module_reset(1, MODULE_RESET_DSP_0 | MODULE_RESET_DSP_1);
+	/* check CPLD */
+	printf("\neDPU CPLD:%s\n", edpu_cpld_version());
+	edpu_init_eth_mac(); /* read and set ps ethernet mac address from CPLD*/
+
+	/* check DSP's I2C EEPROM */
+	edpu_module_reset(1, MODULE_RESET_DSP_0 | MODULE_RESET_DSP_1); /* put all dsp into reset */
 	udelay(1*1000);
-	
+
 	/* configure dsp i2c eeprom if need */
 	//edpu_dsp_config_clear(0);
 	edpu_dsp_config(0, 0);
@@ -768,7 +958,4 @@ void edpu_cpld_init(void)
 
 	/* release dsp */
 	edpu_module_reset(0, MODULE_RESET_DSP_0 | MODULE_RESET_DSP_1);
-
-	/* read ps ethernet mac address */
-	edpu_init_eth_mac();
 }
--- include/configs/xilinx_zynqmp_edpu.h
+++ include/configs/xilinx_zynqmp_edpu.h
@@ -31,15 +31,41 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"image_addr=0x10000000\0" \
 	"scsidev=0\0" \
-	"fpga_img=fpga.bit\0" \
 	"fpga_idx=0\0" \
-	"kernel_img=image.ub\0" \
-	"sataboot=scsi scan && " \
-		"if test -e scsi $scsidev $fpga_img; then " \
-			"load scsi $scsidev $image_addr $fpga_img && fpga loadb $fpga_idx $fileaddr $filesize; " \
+	"generate_file_names=" \
+		"if test -e $ddr_pl_size; then " \
+			"setenv pl_image_file plm\"$ddr_pl_size\"r\"$ddr_pl_rank\".bit; " \
 		"fi; " \
-		"load scsi $scsidev $image_addr $kernel_img && " \
-		"bootm $fileaddr\0"
+		"if test -e $ddr_ps_size; then " \
+			"setenv ps_image_file psm\"$ddr_ps_size\"r\"$ddr_ps_rank\".ub; " \
+		"fi; \0" \
+	"load_pl=" \
+		"env delete fileaddr; " \
+		"if test -e scsi $scsidev $pl_image_file; then " \
+			"load scsi $scsidev $image_addr $pl_image_file; " \
+		"else " \
+			"if test -e scsi $scsidev fpga.bit; then " \
+				"load scsi $scsidev $image_addr fpga.bit; "\
+			"fi; " \
+		"fi; " \
+		"if test -e $fileaddr; then " \
+			"fpga loadb $fpga_idx $fileaddr $filesize; " \
+		"fi; \0" \
+	"load_ps=" \
+		"env delete fileaddr; " \
+		"if test -e scsi $scsidev $ps_image_file; then " \
+			"load scsi $scsidev $image_addr $ps_image_file; " \
+		"else " \
+			"load scsi $scsidev $image_addr image.ub; "\
+		"fi; " \
+		"if test -e $fileaddr; then " \
+			"bootm $fileaddr; " \
+		"fi; \0" \
+	"sataboot=scsi scan && " \
+		"run generate_file_names || " \
+		"run load_pl || ; " \
+		"run load_ps; \0" \
+		
 #include <configs/xilinx_zynqmp.h>
 
 #endif /* __CONFIG_ZYNQMP_EDPU_H */
